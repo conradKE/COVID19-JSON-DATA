6072cdfb4a62947d6e84bc5352f0c439ca38f8a7
azim254 <verdixconie@gmail.com>
2020-03-28 15:11:19 +0300

    initial commit

---
+++ data.php
@@ -0,0 +1,55 @@
+<?php
+
+
+header('Content-type: application/json');
+
+// Set your CSV feed
+$cases = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv';
+
+// Arrays we'll use later
+$keys = array();
+$newArray = array();
+
+// Function to convert CSV into associative array
+function csvToArray($file, $delimiter) { 
+  if (($handle = fopen($file, 'r')) !== FALSE) { 
+    $i = 0; 
+    while (($lineArray = fgetcsv($handle, 4000, $delimiter, '"')) !== FALSE) { 
+      for ($j = 0; $j < count($lineArray); $j++) { 
+        $arr[$i][$j] = $lineArray[$j]; 
+      } 
+      $i++; 
+    } 
+    fclose($handle); 
+  } 
+  return $arr; 
+} 
+
+// data to array
+$data = csvToArray($cases, ',');
+
+// Set number of elements (minus 1 because we shift off the first row)
+$count = count($data) - 1;
+  
+//Use first row for names  
+$labels = array_shift($data);  
+
+foreach ($labels as $label) {
+  $keys[] = $label;
+}
+
+// Add Ids to the countries we might use them later
+$keys[] = 'id';
+
+for ($i = 0; $i < $count; $i++) {
+  $data[$i][] = $i;
+}
+  
+// Bring it all together
+for ($j = 0; $j < $count; $j++) {
+  $d = array_combine($keys, $data[$j]);
+  $newArray[$j] = $d;
+}
+
+// Print it out as JSON
+echo json_encode($newArray,JSON_PRETTY_PRINT,JSON_UNESCAPED_SLASHES);
